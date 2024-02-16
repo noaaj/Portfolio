f0cd27da562c361962140cafad6cdc1ac3ab6630
Angel Noa <xangelnoa@gmail.com>
2024-02-16 14:23:16 -0500

    Compile Error

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -7,10 +7,10 @@ export function Home() {
             animate={motionValues[1]}
             exit={motionValues[2]}
             transition={motionValues[3]}>
-            <h2>I'm a Front End Developer</h2>
+            <h2>I&aposm a Front End Developer</h2>
             <h3>I design and code digital experiences.</h3>
-            <p>I am a web developer based in Orlando, Florida. I focus on front end development using HTML, CSS, and Javascript to create websites and web apps. I incorporate development libraries for projects as needed. I'm always open to using new technologies and skills that fit a specific project.</p>
-            <p>My web development journey started at the University of Central Florida in 2012. I graduated from there with a degree in Web Design in 2016. After school, I worked at BryteBridge for 6 years. While there I would develop hundreds of websites for our clients, and help with multitude of our company's websites.</p>
+            <p>I am a web developer based in Orlando, Florida. I focus on front end development using HTML, CSS, and Javascript to create websites and web apps. I incorporate development libraries for projects as needed. I&aposm always open to using new technologies and skills that fit a specific project.</p>
+            <p>My web development journey started at the University of Central Florida in 2012. I graduated from there with a degree in Web Design in 2016. After school, I worked at BryteBridge for 6 years. While there I would develop hundreds of websites for our clients, and help with multitude of our company&aposs websites.</p>
             <p>I focus on learning new technologies and building projects. That work is showcased here.</p>
         </motion.section>
     );
